e4295e3f4080d2c9a56ed9ce793e1f2c0597c2ee
AthensAphirak <aphirak.ztll@gmail.com>
2021-08-20 16:27:27 +0700

    add new header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,6 +11,15 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">New Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">All Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Best Seller Product</a>
+        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
